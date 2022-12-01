cfb5c343c59f35d05e1d41ab17adb6f751da19e7
Audrey LOURI <audrey.louri@gmail.com>
2022-12-01 23:45:04 +0100

    add burger menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,44 @@
+import { useState } from 'react';
 import logo from '../../assets/images/logo.svg';
 
 import './style.scss';
 
 const Header = () => {
+  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOnClick = ():void => {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <header className="header">
-      <div className="header__logo">
-        <img src={logo} alt="Shortly logo" />
+  <header className="header">
+    <div className="header__logo">
+      <img src={logo} alt="Shortly logo" />
+    </div>
+    <button
+      className="header__burger"
+      type="button"
+      onClick={handleOnClick}
+    >
+      <div></div>
+      <div></div>
+      <div></div>
+    </button>
+    <nav className={isOpen ? "header__navbar--responsive" : "header__navbar"}>
+      <div>
+        <ul>
+          <li><a href="/">Features</a></li>
+          <li><a href="/">Pricing</a></li>
+          <li><a href="/">Resources</a></li> 
+        </ul>
       </div>
-      <nav className="header__navbar">
-        <div>
-          <ul>
-            <li><a href="/">Features</a></li>
-            <li><a href="/">Pricing</a></li>
-            <li><a href="/">Resources</a></li> 
-          </ul>
-        </div>
-        <div className="header__navbar--connection">
-          <a href="/">Login</a>
-          <button type="button">Sign Up</button>
-        </div>
-      </nav>
-    </header>
+      <div className="header__connection">
+        <a href="/">Login</a>
+        <button type="button">Sign Up</button>
+      </div>
+    </nav>
+  </header>
   )
 };
 
