5a909b720d60b3d53b1a7a3562834135b5e1bd4b
Audrey LOURI <audrey.louri@gmail.com>
2022-12-07 19:54:33 +0100

    add responsive features for mobile and tablet

--- src/components/Features/Content/index.tsx
+++ src/components/Features/Content/index.tsx
@@ -5,20 +5,20 @@ import customizable_icon from '../../../assets/images/icon-fully-customizable.sv
 import './style.scss';
 
 const Content = () => {
-  return(
+  return (
     <div className="content">
       <div className="content__section">
-        <div  className="content__icon">
+        <div className="content__icon">
           <img src={recognition_icon} alt="icon brand recognition" />
         </div>
         <h5 className="content__title">Brand Recognition</h5>
-        <p className="content__text">Boost your brand recognition with eacg click.
+        <p className="content__text">Boost your brand recognition with each click.
           Generic links don't mean a thing. Branded links help
           instil confidence in your content.
         </p>
       </div>
       <div className="content__section">
-        <div  className="content__icon">
+        <div className="content__icon">
           <img src={records_icon} alt="icon-detailed-records" />
         </div>
         <h5 className="content__title">Detailed Records</h5>
@@ -28,7 +28,7 @@ const Content = () => {
         </p>
       </div>
       <div className="content__section">
-        <div  className="content__icon">
+        <div className="content__icon">
           <img src={customizable_icon} alt="icon fully customizable" />
         </div>
         <h5 className="content__title">Fully Customizable</h5>
--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -8,7 +8,6 @@ const Features = () => {
       <h3 className="features__title">Advanced Statistics</h3>
       <p className="features__text">
         Track how your links are performing across the web with
-        <br />
         our advanced statistics dashboard.
       </p>
       <Content />
